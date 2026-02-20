d54574dab0f5a0421297e764b5d44c43b739c9d5
RicardoFrancoDelgado <rf041555@gmail.com>
2026-02-20 20:32:54 -0300

    feat: create project and create contact class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import com.ricardo.agenda.model.Contato;
+
+class Main {
+    public static void main(String[] args) {
+        Contato c1 = new Contato("Ricardo", "21 91234-5678", "ricardo@email.com");
+
+
+    }
+}
